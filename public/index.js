58e9dcbfeb1d5563b82a77be49ea5dc27d426627
Claudia Belanger <claudiabelanger@Claudias-iMac.local>
2022-12-12 12:55:46 -0500

    update error in index.js file

--- public/index.js
+++ public/index.js
@@ -1,45 +1,6 @@
 
 $(document).ready(function(){
-  
-    // //hamburger menu navbar
-    // const headerNav = document.querySelector('.header-nav');
-    // const menu = document.querySelector('.menu-btn');
-    // const mobileTransparent= document.querySelector('.transparent');
-    // const mobileWhite= document.querySelector('.white');
-    // const mobileHeader = document.querySelector('.header');
-    // const logoWhite = document.querySelector('.navbar-logo-white');
-    // const logoBlack = document.querySelector('.navbar-logo-black');
-
-    // menu.addEventListener('click', () => {
-    //     headerNav.classList.toggle('mobile-menu-active');
-    //     if(mobileTransparent){
-    //         mobileHeader.classList.toggle('mobile-menu-active');
-    //         menu.classList.toggle('close');
-    //         logoWhite.classList.toggle('visible');
-    //         logoBlack.classList.toggle('visible');
-    //         console.log('hello')
-    //         return;
-    //     }
-    //     else if(mobileWhite){
-    //         mobileHeader.classList.toggle('mobile-menu-active');
-    //         menu.classList.toggle('close');
-    //         // logoWhite.classList.toggle('visible');
-    //         // logoBlack.classList.toggle('visible');
-    //     }
-    //     else {
-    //         console.log('hi')
-    //         mobileHeader.classList.toggle('mobile-menu-active');
-    //         menu.classList.toggle('close');
-    //         // logoWhite.classList.toggle('visible');
-    //         // logoBlack.classList.toggle('visible');
-    //     }             
-
-    // })
-
-
-})
-   
-
+     
     //fade animation
     const fadeInLeft = document.querySelectorAll('.fade-in-left');
     const fadeIn = document.querySelectorAll('.fade-in');
@@ -78,36 +39,8 @@ $(document).ready(function(){
         appearOnScroll.observe(fade)
     })
 
+})
+
+
   
 
-
-
-// let current = 0;
-// let target = 0;
-// let ease = 0.4;
-
-// let windowWidth, containerHeight;
-
-// let scrollWrap = document.getElementsByClassName("smooth-scroll-wrapper")[0];
-
-// function setTransform(el, transform){
-//     el.style.transform = transform;
-// }
-
-// function setUpAnimation(){
-//     containerHeight = scrollWrap.getBoundingClientRect().height;
-//     document.body.style.height = `${containerHeight}px`  
-//     smoothScroll();
-// }
-
-// function smoothScroll(){
-//     current = current * (1 - ease) + target * ease;
-//     current = parseFloat(current.toFixed(2));
-//     target = window.scrollY;
-
-//     setTransform(scrollWrap, `translateY(${-current * ease }px)`)
-//     window.requestAnimationFrame(smoothScroll)
-// }
-
-
-// setUpAnimation();
